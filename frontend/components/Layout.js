b429538d2ddaa86314d9813441a248f5e6b03e6e
Alex M <1maltsevalex@gmail.com>
2021-08-22 16:05:59 +0300

    4->head Tags & Layout

---
+++ frontend/components/Layout.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import styles from '../styles/Layout.module.css';
+
+export default function Layout({ title, keywords, description, children }) {
+ return (
+  <div>
+   <Head>
+    <title>{title}</title>
+    <meta name='description' content='{description}' />
+    <meta name='keywords' content='{keywords}' />
+   </Head>
+   <div className={styles.container}>{children}</div>
+  </div>
+ );
+}
+
+Layout.defaultProps = {
+ title: 'בניית ציפורניים | לק גל',
+ description:
+  'בניית ציפורניים קצרהארוכה 250₪₪280 | לק ג&#039;ל 120₪ | מילוי 130₪ | בנייה בפוליגל 280₪ | הסרת שער בלייזר 300₪550₪ | להזמנות חייגו עכשיו 054-800-2334',
+ keywords:
+  'בניית ציפורניים קצרהארוכה 250₪₪280 | לק ג&#039;ל 120₪ | מילוי 130₪ | בנייה בפוליגל 280₪ | הסרת שער בלייזר 300₪550₪ | להזמנות חייגו עכשיו 054-800-2334',
+};
--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,11 +1,11 @@
-import Link from 'next/link';
+import Layout from '../components/Layout';
 
 export default function about() {
  return (
-  <div>
+  <Layout title='בניית ציפורניים | לק גל'>
    <h1>About</h1>
    <p>This is an app to find the latest DJ and other musical events</p>
    <p>Version: 1.0.0</p>
-  </div>
+  </Layout>
  );
 }
